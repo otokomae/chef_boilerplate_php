aa6be2e1e0a8197d3c840f421df1bc984ffe90f0
topaz2 <topaz2.3333@gmail.com>
2014-09-03 17:13:00 +0900

    Fixed rubocop errors

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,40 +1,40 @@
 default[:boilerplate_php][:git] = {
-  :hooks => {
-    :lint => 'php/lint',
-    :phpcs => 'php/phpcs',
-    :phpmd => 'php/phpmd',
-    :phpunit => 'php/phpunit'
+  hooks: {
+    lint: 'php/lint',
+    phpcs: 'php/phpcs',
+    phpmd: 'php/phpmd',
+    phpunit: 'php/phpunit'
   }
 }
 
 default[:boilerplate_php][:framework] = {
-  :type => :symfony
+  type: :symfony
 }
 default[:boilerplate_php][:symfony] = {
-  :version => '2.x',
-  :git => {
-    :hooks => {
+  version: '2.x',
+  git: {
+    hooks: {
     }
   }
 }
 default[:boilerplate_php][:cakephp] = {
-  :version => '2.x',
-  :git => {
-    :hooks => {
-      :phpcs => 'cakephp/phpcs',
-      :phpunit => 'cakephp/phpunit'
+  version: '2.x',
+  git: {
+    hooks: {
+      phpcs: 'cakephp/phpcs',
+      phpunit: 'cakephp/phpunit'
     }
   }
 }
 default[:boilerplate_php][:sphinx] = {
-  :host => 'sphinx.local',
-  :port => '80',
-  :path => '/'
+  host: 'sphinx.local',
+  port: '80',
+  path: '/'
 }
 default[:boilerplate_php][:phpdoc] = {
-  :host => 'phpdoc.local',
-  :port => '80',
-  :path => '/'
+  host: 'phpdoc.local',
+  port: '80',
+  path: '/'
 }
 
 # Merge framework specific hooks
