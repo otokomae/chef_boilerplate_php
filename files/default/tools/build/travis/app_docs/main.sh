c2dd03872fc72ab96d4cb354a88c79be194ee714
topaz2 <topaz2.3333@gmail.com>
2014-09-09 18:45:56 +0900

    Made docs validation more strict

--- files/default/tools/build/travis/app_docs/main.sh
+++ files/default/tools/build/travis/app_docs/main.sh
@@ -1,6 +1,35 @@
 #!/bin/bash -ex
 
-cd $TRAVIS_BUILD_DIR/sphinx && make html
-cd $TRAVIS_BUILD_DIR && git clone https://github.com/NetCommons3/NetCommons3.git
-export APP_ROOT=$TRAVIS_BUILD_DIR/NetCommons3/app
-cd $TRAVIS_BUILD_DIR && phpdoc -d $APP_ROOT -t $TRAVIS_BUILD_DIR/phpdoc/ -i "$APP_ROOT/Plugin/DebugKit/*,$APP_ROOT/Plugin/Migrations/*,$APP_ROOT/Plugin/MobileDetect/*,$APP_ROOT/Config/*,$APP_ROOT/Plugin/Public/*,$APP_ROOT/Test/*"
+APP=`basename $CLASS_DOC_SOURCE_URI | perl -pi -e 's/([^\/]+)\.git$/$1/'`
+CLASS_DOC_SOURCE_ROOT=$WORKSPACE/$APP
+APP_ROOT=$CLASS_DOC_SOURCE_ROOT/app
+LOG=/var/log/phpdoc.log
+
+# Clear previous build
+rm -rf $APP
+cat /dev/null > $LOG
+
+# Generate sphinx docs
+cd $WORKSPACE/sphinx
+make html
+cd -
+
+# Install all plugins
+git clone $CLASS_DOC_SOURCE_URI
+cd $CLASS_DOC_SOURCE_ROOT
+cp tools/build/app/cakephp/composer.json .
+hhvm `which composer` update --prefer-dist
+
+# Init phpdoc options
+for p in `cat app/Config/vendors.txt`
+do
+  IGNORE_PLUGINS="$IGNORE_PLUGINS,*/app/Plugin/$p/*"
+done
+IGNORE_PLUGINS=`echo $IGNORE_PLUGINS | cut -c 2-`
+
+# Exit on parse error
+phpdoc parse -d $APP_ROOT -t $WORKSPACE/phpdoc -i $IGNORE_PLUGINS,*/Config/* --force --ansi >> $LOG
+[ `grep -c '\[37;41m' $LOG` -ne 0 ] && cat $LOG && exit 1
+
+# Generate class docs
+phpdoc -d $APP_ROOT -t $WORKSPACE/phpdoc -i $IGNORE_PLUGINS,*/Config/*
--- files/default/tools/build/travis/app_docs/pre.sh
+++ files/default/tools/build/travis/app_docs/pre.sh
@@ -1,14 +1,5 @@
 #!/bin/bash -ex
 
-if [ "$ENVIRONMENT" = "development" ]
-then
-  bundle update
-  bundle ex berks update
-else
-  bundle install --without development
-  bundle ex berks install -e development
-fi
-
 sudo apt-get install graphviz
 sudo pip install sphinx
 mkdir -p build/logs
@@ -16,3 +7,5 @@ pear channel-discover pear.phpdoc.org
 pear install phpdoc/phpDocumentor
 phpenv rehash
 set +H
+
+WORKSPACE=$TRAVIS_BUILD_DIR
