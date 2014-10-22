91d9976020a04222f148467b00445f63ede67fee
topaz2 <topaz2.3333@gmail.com>
2014-10-22 17:22:41 +0900

    Disabled cookbook upload

--- files/default/tools/build/jenkins/chef_boilerplate_php/main.sh
+++ files/default/tools/build/jenkins/chef_boilerplate_php/main.sh
@@ -4,4 +4,4 @@ foodcritic -f any --tags ~FC001 --tags ~FC014 .
 rspec --color --format progress
 rubocop
 kitchen test --destroy=always -c `ohai cpu/total`
-knife cookbook site share boilerplate_php Utilities -u topaz2
+# knife cookbook site share boilerplate_php Utilities -u topaz2
